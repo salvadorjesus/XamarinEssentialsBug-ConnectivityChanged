967881449c0b077564622b1dbc088ccde650e32b
Salvador Jesús Romero Castellano <salvadorjesus@gmail.com>
2024-02-26 13:03:48 +0100

    Gitignore file renamed. Projet files.

---
+++ ConnectivityBug/ConnectivityBug/ConnectivityBug/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ConnectivityBug
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+
+            Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
+        }
+
+        private async void Connectivity_ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+        {
+            Console.WriteLine("Connectivity changed");
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
